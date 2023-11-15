f5e8dc2c0cbb2189eb18f5c3f6b076f2d7965e69
dimas rizki <150767523+DRZ8@users.noreply.github.com>
2023-11-15 07:38:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Biodata Diri</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <h1>Biodata Diri</h1>
+      <img src="foto.jpg" alt="Foto Diri" />
+      <div class="info">
+        <h2>Data Pribadi</h2>
+        <p>Nama Lengkap: Dimas Rizki</p>
+        <p>Tempat, Tanggal Lahir: Sumedang, 18 maret 2005</p>
+        <p>Alamat: dsn. ciumpleng desa cinansi kec. cisitu kab. sumedang</p>
+        <p>Email: dimasriz009@gmail.com</p>
+        <p>Telepon: 081952009838</p>
+        <h2>Pendidikan</h2>
+        <ul>
+          <li>SDN Pabuaran </li>
+          <li>SMPN 1 Cisitu </li>
+          <li>SMK Negeri Situraja </li>
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
